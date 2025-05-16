aa4229ef92d3b5a93e3bb59bb81ee107398ce0d5
Buryat <59414511+BuryakDuryak@users.noreply.github.com>
2025-05-16 12:50:06 +0500

    Delete src directory

--- src/main/kotlin/ru/therasync/therasync_chat/model/MessageStatus.kt
+++
@@ -1,5 +0,0 @@
-package ru.therasync.therasync_chat.model
-
-enum class MessageStatus {
-    SENT, DELIVERED, READ
-}
--- src/main/kotlin/ru/therasync/therasync_chat/repository/ChatMessageRepository.kt
+++
@@ -1,8 +0,0 @@
-package ru.therasync.therasync_chat.repository
-
-import ru.therasync.therasync_chat.model.ChatMessage
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ChatMessageRepository : JpaRepository<ChatMessage, Long> {
-    fun findByRoomIdOrderByTimestampAsc(roomId: Long): List<ChatMessage>
-}
